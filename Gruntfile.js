70916e98892e26289048676ab03219a430565d51
David Shanske <david@shanske.com>
2018-04-14 11:19:44 -0500

    Exclude vendor directory from grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,6 +44,7 @@ module.exports = function(grunt) {
                src: [
                    '**',
                    '!node_modules/**',
+		   '!vendor/**',
                    '!build/**',
                    '!.git/**',
                    '!Gruntfile.js',
